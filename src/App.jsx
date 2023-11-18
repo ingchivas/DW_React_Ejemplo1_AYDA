e38c3732632ed98fd6747305de1cce8b1f7615c9
Alberto L. Figueroa <0234621@up.edu.mx>
2023-11-18 09:12:13 -0600

    FirstCommit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hola esto es una prueba de react/vite</h1>
+    </>
+  )
+}
+
+export default App
